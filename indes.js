3a3d1ba6d70833e1dcb26333df51515a0703bcca
David <cruz-azul99@hotmail.com>
2021-02-20 09:42:26 -0600

    Function added

--- indes.js
+++ indes.js
@@ -1,6 +1,19 @@
+let array = [1,32,3,14]
+let another_array = [1,14]
+
 function isValidSubsequence(array, sequence) {
-  // Write your code here.
+  let index = 0
+  
+  for(el of array) {
+    if(el === sequence[index]) {
+      index++
+    }
+  }
+
+  return index === sequence.length
 }
+
+console.log(isValidSubsequence(array,another_array))
 
 // Do not edit the line below.
 exports.isValidSubsequence = isValidSubsequence;
